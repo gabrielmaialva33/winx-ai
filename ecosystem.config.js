47dabaff66b626d21404a9bef50c3761e0cff5e5
Gabriel Maia <gabrielmaialva33@gmail.com>
2023-02-07 04:11:22 -0300

    chore: update readme

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'winx',
-      command: 'npm run build && npm run start',
+      command: 'rimraf dist && npm run build && npm run start',
       restart: true,
     },
   ],

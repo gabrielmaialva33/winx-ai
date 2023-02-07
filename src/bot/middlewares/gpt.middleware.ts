e3067ac9cefc08ee4183fc745937191bee7c894c
Gabriel Maia <gabrielmaialva33@gmail.com>
2023-02-07 14:59:17 -0300

    chore: update readme

--- src/bot/middlewares/gpt.middleware.ts
+++ src/bot/middlewares/gpt.middleware.ts
@@ -6,7 +6,6 @@ import { StringUtils } from '@/helpers/string.utils'
 import { GptUtils } from '@/helpers/gpt.utils'
 import { HistoryUtils } from '@/helpers/history.utils'
 import { IA } from '@/bot/plugins/gpt.plugin'
-import { User } from '@/main'
 
 export const gpt: MiddlewareFn = async (ctx, next) => {
   try {
@@ -24,7 +23,7 @@ export const gpt: MiddlewareFn = async (ctx, next) => {
     ) {
       const input = GptUtils.build_input({ text, username, reply_to_username, reply_to_text })
 
-      //await ctx.api.sendChatAction(ctx.chat!.id, 'typing')
+      await ctx.api.sendChatAction(ctx.chat!.id, 'typing')
 
       const response = await IA.complete(input, username)
       if (!response.data.choices[0].text) return next()
@@ -33,24 +32,23 @@ export const gpt: MiddlewareFn = async (ctx, next) => {
       const history = HistoryUtils.build_gpt_history(input, output, username)
       HistoryUtils.write_history(history)
 
-      await User.sendMessage(
-        ctx.chat.id,
-        response.data.choices[0].text + '\n',
-        ctx.message.message_id
-      )
+      return ctx.reply(response.data.choices[0].text + '\n', {
+        reply_to_message_id: ctx.message.message_id,
+      })
+      // await User.sendMessage(
+      //   ctx.chat.id,
+      //   response.data.choices[0].text + '\n',
+      //   ctx.message.message_id
+      // )
 
-      return next()
-
-      // return ctx.reply(response.data.choices[0].text + '\n', {
-      //   reply_to_message_id: ctx.message.message_id,
-      // })
+      // return next()
     }
 
     // ctx.message.reply_to_message?.from?.id === ctx.me.id
     if (ctx.message.reply_to_message?.from?.id === 5635583594) {
       const input = GptUtils.build_input({ text, username, reply_to_username, reply_to_text })
 
-      //await ctx.api.sendChatAction(ctx.chat!.id, 'typing')
+      await ctx.api.sendChatAction(ctx.chat!.id, 'typing')
 
       const response = await IA.complete(input, username)
       if (!response.data.choices[0].text) return next()
@@ -59,16 +57,17 @@ export const gpt: MiddlewareFn = async (ctx, next) => {
       const history = HistoryUtils.build_reply_gpt_history(input, output, username)
       HistoryUtils.write_history(history)
 
-      // return ctx.reply(response.data.choices[0].text + '\n', {
-      //   reply_to_message_id: ctx.message.message_id,
-      // })
-      await User.sendMessage(
-        ctx.chat.id,
-        response.data.choices[0].text + '\n',
-        ctx.message.message_id
-      )
+      return ctx.reply(response.data.choices[0].text + '\n', {
+        reply_to_message_id: ctx.message.message_id,
+      })
 
-      return next()
+      // await User.sendMessage(
+      //   ctx.chat.id,
+      //   response.data.choices[0].text + '\n',
+      //   ctx.message.message_id
+      // )
+
+      // return next()
     }
   } catch (error) {
     Logger.error(error, 'MIDDLEWARE/GPT')

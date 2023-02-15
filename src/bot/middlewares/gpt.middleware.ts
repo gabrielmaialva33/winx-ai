643ccde40ad3c481a137f16c304c68264fd32628
Gabriel Maia <gabrielmaialva33@gmail.com>
2023-02-15 17:17:06 -0300

    chore: update libs

--- src/bot/middlewares/gpt.middleware.ts
+++ src/bot/middlewares/gpt.middleware.ts
@@ -56,11 +56,7 @@ export const gpt: MiddlewareFn = async (ctx, next) => {
     }
 
     // random reply
-    if (
-      Math.random() < 0.5 &&
-      !ctx.message.reply_to_message &&
-      !StringUtils.text_includes(text, ['/imagine', '/variation', '/'])
-    ) {
+    if (Math.random() < 0.5 && !StringUtils.text_includes(text, ['/imagine', '/variation', '/'])) {
       const input = GptUtils.build_input({ text, username, reply_to_username, reply_to_text })
 
       Logger.info(input, 'MIDDLEWARE/GPT/RANDOM')

416516deff5193ee0f25b9e1acf1f43bf74ece07
Gabriel Maia <gabrielmaialva33@gmail.com>
2023-02-17 10:16:54 -0300

    chore: improvements update

--- src/bot/plugins/gpt.plugin.ts
+++ src/bot/plugins/gpt.plugin.ts
@@ -35,8 +35,6 @@ class OpenAI extends OpenAIApi {
         prompt,
         temperature: 0.6,
         max_tokens: 400,
-        frequency_penalty: 1.5,
-        presence_penalty: 1.5,
         stop: ['|'],
       })
     }
@@ -46,8 +44,6 @@ class OpenAI extends OpenAIApi {
       prompt,
       temperature: 0.6,
       max_tokens: 400,
-      frequency_penalty: 1.5,
-      presence_penalty: 1.5,
       stop: ['|'],
     })
   }

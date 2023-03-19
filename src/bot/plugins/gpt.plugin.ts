c4d4b136ed524bd77736448b9004bfb66fe2ef1d
Gabriel Maia <gabrielmaialva33@gmail.com>
2023-03-19 15:52:32 -0300

    chore: update libs

--- src/bot/plugins/gpt.plugin.ts
+++ src/bot/plugins/gpt.plugin.ts
@@ -22,8 +22,8 @@ class OpenAI extends OpenAIApi {
 
   private RandonCompletionRequest = {
     model: 'text-davinci-003',
-    temperature: Math.random() * (0.4 - 0.1) + 0.1,
-    max_tokens: 100,
+    temperature: Math.random() * (0.6 - 0.1) + 0.1,
+    max_tokens: 400,
     frequency_penalty: Math.random() * (0.5 - 0.2) + 0.2,
     presence_penalty: Math.random() * (0.2 - 0.1) + 0.1,
     n: Math.floor(Math.random() * (5 - 1) + 1),

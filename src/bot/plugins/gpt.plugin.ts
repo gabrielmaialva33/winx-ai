41c58cb62552658b7051a96e04acd0a9ed5eaf98
Gabriel Maia <gabrielmaialva33@gmail.com>
2023-05-29 04:41:59 -0300

    feat: update libs

--- src/bot/plugins/gpt.plugin.ts
+++ src/bot/plugins/gpt.plugin.ts
@@ -22,7 +22,7 @@ class OpenAI extends OpenAIApi {
   }
 
   private RandonCompletionRequest = {
-    model: 'text-davinci-002',
+    model: 'text-davinci-003',
     temperature: 1,
     max_tokens: 500,
     frequency_penalty: 1.5,

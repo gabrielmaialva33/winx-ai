7da64121d5e0476d57bb3bf8df9d5bb156e522fd
Gabriel Maia <gabrielmaialva33@gmail.com>
2023-07-11 23:56:39 -0300

    refactor: gpt

--- src/bot/middlewares/history.middleware.ts
+++ src/bot/middlewares/history.middleware.ts
@@ -4,9 +4,12 @@ import { Logger } from '@/helpers/logger.utils'
 import { StringUtils } from '@/helpers/string.utils'
 import { ContextUtils } from '@/helpers/context.utils'
 import { HistoryUtils } from '@/helpers/history.utils'
+import Env from '@/config/env'
 
 export const history: MiddlewareFn = async (ctx, next) => {
   if (!ctx.chat || !ctx.message) return next()
+
+  const permitted = Env.GROUP_ID.split(',').map((id: string) => id.trim())
 
   try {
     if (
@@ -15,7 +18,8 @@ export const history: MiddlewareFn = async (ctx, next) => {
       ctx.message.from.is_bot ||
       ctx.message.reply_to_message?.from?.is_bot ||
       !ctx.message.from?.first_name ||
-      StringUtils.TextInclude(ctx.message!.text!, ['winx', '/'])
+      StringUtils.TextInclude(ctx.message!.text!, ['winx', '/']) ||
+      !permitted.includes(ctx.chat.id.toString())
     ) {
       Logger.debug('ignoring message', 'history.middleware')
       return next()

3812d0c272e69c4ac8dccd6125fa305ddd73381d
Gabriel Maia <gabrielmaialva33@gmail.com>
2023-07-25 00:35:26 -0300

    chore: add rustfmt lib

--- src/bot/plugins/gpt.plugin.ts
+++ src/bot/plugins/gpt.plugin.ts
@@ -63,12 +63,12 @@ class OpenAI extends OpenAIApi {
     const n = this.n[model][Math.floor(Math.random() * this.n[model].length)]
 
     return {
-      model: 'text-davinci-002',
+      model: 'text-davinci-003',
       temperature: 1,
-      max_tokens: 80,
-      frequency_penalty: 2,
-      presence_penalty: 1.5,
-      n: 10,
+      frequency_penalty: 1.5,
+      presence_penalty: 1.0,
+      n: 1,
+      max_tokens: 200,
     } as CreateCompletionRequest
   }
 

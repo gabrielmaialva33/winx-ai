00a09098593feb9b86af510fea8ccd8c4385d55c
Gabriel Maia <gabrielmaialva33@gmail.com>
2023-07-25 01:44:18 -0300

    chore: add rustfmt lib

--- src/bot/core/user.bot.ts
+++ src/bot/core/user.bot.ts
@@ -33,7 +33,7 @@ export class UserBot {
     const chatMessages = await this.user.getMessages(group[0], {
       filter: new Api.InputMessagesFilterEmpty(),
       reverse: false,
-      limit: 70,
+      limit: 1,
     })
     const messages = chatMessages.reverse()
     let context = ''

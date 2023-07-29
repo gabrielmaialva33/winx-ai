e50553c450acc1d8d79cdf96fa916b17041e5800
Gabriel Maia <gabrielmaialva33@gmail.com>
2023-07-29 15:19:43 -0300

    feat: merge stellet

--- src/bot/plugins/gpt.plugin.ts
+++ src/bot/plugins/gpt.plugin.ts
@@ -15,11 +15,11 @@ class OpenAI extends OpenAIApi {
 
   private CompletionRequest(): CreateCompletionRequest {
     return {
-      model: 'text-davinci-002',
+      model: 'text-davinci-003',
       temperature: 1,
       max_tokens: 256,
-      frequency_penalty: 1.8,
-      presence_penalty: 1.4,
+      frequency_penalty: 0.5,
+      presence_penalty: 0.5,
       n: 1,
     }
   }

03b0a97f7e3acf561801f310f58fe59d7969061e
Gabriel Maia <gabrielmaialva33@gmail.com>
2023-08-01 15:47:03 -0300

    chore: update libs

--- src/bot/plugins/gpt.plugin.ts
+++ src/bot/plugins/gpt.plugin.ts
@@ -16,9 +16,9 @@ class OpenAI extends OpenAIApi {
   private CompletionRequest(): CreateCompletionRequest {
     return {
       model: 'text-davinci-003',
-      temperature: 0.7,
+      temperature: 0.6,
       max_tokens: 256,
-      frequency_penalty: 0.5,
+      frequency_penalty: 1.0,
       presence_penalty: 0.5,
       n: 1,
     }

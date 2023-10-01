0bb19d10db00175f4ba58b64be1dc18ae9f65645
Gabriel Maia <gabrielmaialva33@gmail.com>
2023-09-30 23:14:36 -0300

    chore: update requirements

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'winx',
-      command: 'pnpm run build && pnpm run start',
+      command: 'pnpm start:dev',
       restart: true,
       cron_restart: '0 */3 * * *', // Every 3 hours
     },

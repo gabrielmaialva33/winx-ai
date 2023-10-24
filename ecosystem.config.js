86a34e5c12f46699e550720e0c342d790d8f8c20
Gabriel Maia <gabrielmaialva33@gmail.com>
2023-10-24 18:43:58 -0300

    chore: bump libs

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'winx',
-      command: 'yarn start:dev',
+      command: 'pnpm start:dev',
       restart: true,
       cron_restart: '0 */3 * * *', // Every 3 hours
     },

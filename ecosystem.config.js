0b0204b0cf6f9fa76a6fdd3c96ce69cfaa8b4dde
Gabriel Maia <gabrielmaialva33@gmail.com>
2023-11-06 23:56:53 -0300

    chore: initial commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'winx',
-      command: 'pnpm start:dev',
+      command: 'yarn start:dev',
       restart: true,
       cron_restart: '0 */3 * * *', // Every 3 hours
     },

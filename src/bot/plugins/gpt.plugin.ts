a72e09074a50191f92c10307a2cce05b34a62cde
Gabriel Maia <gabrielmaialva33@gmail.com>
2023-11-16 13:59:46 -0300

    refactor: imposter

--- src/bot/plugins/gpt.plugin.ts
+++ src/bot/plugins/gpt.plugin.ts
@@ -16,10 +16,10 @@ export class AI extends OpenAI {
   private config = {
     model: 'gpt-3.5-turbo-instruct',
     //model: 'text-davinci-003',
-    temperature: 0.8,
+    temperature: 0.9,
     max_tokens: 256,
-    frequency_penalty: 0.6,
-    presence_penalty: 0.3,
+    frequency_penalty: 0.5,
+    presence_penalty: 0.4,
     n: 1,
     stop: ['||'],
   } as CompletionCreateParamsBase

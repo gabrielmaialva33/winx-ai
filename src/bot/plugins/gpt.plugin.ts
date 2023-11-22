836b1f4ba2ed7f3a4da405c257ada5acf9e4871e
Gabriel Maia <gabrielmaialva33@gmail.com>
2023-11-22 14:54:46 -0300

    refactor: dockerfile

--- src/bot/plugins/gpt.plugin.ts
+++ src/bot/plugins/gpt.plugin.ts
@@ -16,10 +16,10 @@ export class AI extends OpenAI {
   private config = {
     model: 'gpt-3.5-turbo-instruct',
     //model: 'text-davinci-003',
-    temperature: 0.8,
+    temperature: 0.9,
     max_tokens: 256,
-    frequency_penalty: 0.6,
-    presence_penalty: 0.4,
+    frequency_penalty: 0.2,
+    presence_penalty: 0.1,
     n: 1,
     stop: ['||'],
   } as CompletionCreateParamsBase

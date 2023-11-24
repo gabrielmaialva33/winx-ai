675ecbec1438108aba5c5bd323a88e8d2109b21e
Gabriel Maia <gabrielmaialva33@gmail.com>
2023-11-24 05:31:26 -0300

    refactor: dockerfile

--- src/bot/plugins/gpt.plugin.ts
+++ src/bot/plugins/gpt.plugin.ts
@@ -14,12 +14,12 @@ import { CompletionCreateParamsBase } from 'openai/src/resources/completions'
 
 export class AI extends OpenAI {
   private config = {
-    //model: 'gpt-3.5-turbo-instruct',
-    model: 'text-davinci-002',
+    model: 'gpt-3.5-turbo-instruct',
+    //model: 'text-davinci-002',
     temperature: 1,
     max_tokens: 128,
-    frequency_penalty: 1.2,
-    presence_penalty: 0.9,
+    frequency_penalty: 0.5,
+    presence_penalty: 0.7,
     n: 1,
     stop: ['||'],
   } as CompletionCreateParamsBase

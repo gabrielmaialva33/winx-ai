d2cb8bc2451fe3304e6f057d6ff11b78e0f3103c
Gabriel Maia <gabrielmaialva33@gmail.com>
2023-12-17 02:16:47 -0300

    chore: bump libs

--- src/bot/plugins/gpt.plugin.ts
+++ src/bot/plugins/gpt.plugin.ts
@@ -14,12 +14,12 @@ import { CompletionCreateParamsBase } from 'openai/src/resources/completions'
 
 export class AI extends OpenAI {
   private config = {
-    //model: 'gpt-3.5-turbo-instruct',
-    model: 'text-davinci-002',
-    temperature: 1,
+    model: 'gpt-3.5-turbo-instruct',
+    //model: 'text-davinci-002',
+    temperature: 0.7,
     max_tokens: 128,
-    frequency_penalty: 1.4,
-    presence_penalty: 1.6,
+    frequency_penalty: 0.5,
+    presence_penalty: 0.5,
     n: 1,
     stop: ['||'],
   } as CompletionCreateParamsBase

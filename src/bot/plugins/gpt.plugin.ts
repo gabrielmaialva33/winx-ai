f208779b152a69f72e97df458afca4337164e198
Gabriel Maia <gabrielmaialva33@gmail.com>
2023-12-22 19:11:57 -0300

    docs: add public docs

--- src/bot/plugins/gpt.plugin.ts
+++ src/bot/plugins/gpt.plugin.ts
@@ -16,8 +16,8 @@ export class AI extends OpenAI {
   private config = {
     model: 'gpt-3.5-turbo-instruct',
     //model: 'text-davinci-002',
-    temperature: 1,
-    max_tokens: 256,
+    temperature: 0.5,
+    max_tokens: 128,
     frequency_penalty: 0.5,
     presence_penalty: 1,
     n: 1,

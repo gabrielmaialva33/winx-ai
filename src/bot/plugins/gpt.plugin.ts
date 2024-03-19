2c473797e1289928eb83e2500cf8214044e62546
Gabriel Maia <gabrielmaialva33@gmail.com>
2024-03-19 12:04:07 -0300

    chore: update libs

--- src/bot/plugins/gpt.plugin.ts
+++ src/bot/plugins/gpt.plugin.ts
@@ -17,9 +17,9 @@ export class AI extends OpenAI {
     model: 'gpt-3.5-turbo-instruct',
     //model: 'text-davinci-002',
     temperature: 0.8,
-    max_tokens: 128,
-    frequency_penalty: 0.5,
-    presence_penalty: 0.5,
+    max_tokens: 256,
+    frequency_penalty: 0.2,
+    presence_penalty: 0.3,
     n: 1,
     stop: ['||'],
   } as CompletionCreateParamsBase
